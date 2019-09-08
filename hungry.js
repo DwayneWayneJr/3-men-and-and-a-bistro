f856b10c26326efbbe44b48ff09d51d4566fcb41
DwayneWayneJr <dwaynejgrant@gmail.com>
2019-09-08 16:16:55 -0700

    finished menu

---
+++ hungry.js
@@ -0,0 +1,7 @@
+    // should create a pop-up that creates boolean response to Hungry y/n?
+    var hungry = confirm("Are you ready to view the menu?");
+    if (hungry == true) {
+    alert("Welcome to 3 Men and a Bistro!");
+    } else {
+      alert("Come back later!")
+    }
