f601e6c5e7be81e145adaf3483a95cd9b5ff7d4a
DwayneWayneJr <dwaynejgrant@gmail.com>
2019-09-08 16:21:33 -0700

    added changes to menu and js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 // var today = new Date();
-var hourNow = prompt("What's the hour?");
+var hourNow = prompt("What time is it?(24hr clock)");
 var greeting;
 
 if (hourNow < 12) {
-  greeting = 'Good morning'
-} else if (hourNow < 15) {
-  greeting = 'Good afternoon'
+  greeting = 'Good Morning!';
+} else if (hourNow >= 12 && hourNow <= 17) {
+  greeting = 'Good Afternoon!';
+} else if (hourNow >= 17 && hourNow <= 24) {
+  greeting = 'Good Evening!';
 } else {
-  greeting = 'Welcome'
+  greeting = 'Welcome!'
 }
 document.write('<h3>'+ greeting + '</h3>');
